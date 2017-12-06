36a306edcb6a48ccec6b3f6658a157f8d7921678
Yanny Castrillon <yannyandres117@gmail.com>
2017-12-05 19:10:30 -0800

    Refactor regression functionality and the entry data_set.

--- regression.py
+++ regression.py
@@ -32,8 +32,7 @@ def get_variance(arr_points):
     mean = get_mean(arr_points)
     variance = 0.0
     for x in arr_points:
-        variance = variance + (x - mean) ** 2
-    # variance = total / (len(arr_points) - 1)
+        variance += ((x - mean) ** 2)
     return variance
 
 def get_covariance(x_points, y_points):
@@ -42,7 +41,7 @@ def get_covariance(x_points, y_points):
     total = 0
     i = 0
     while i < len(x_points):
-        total = total + ((x_points[i] - x_mean) * (y_points[i] - y_mean))
+        total += ((x_points[i] - x_mean) * (y_points[i] - y_mean)) / len(x_points)
         i+=1
 
     return total
@@ -51,33 +50,21 @@ def get_coeficients(data_set):
     h = {}
     x_points = []
     y_points = []
+
     print data_set
+
     for row in data_set:
         x_points.append(row[0])
         y_points.append(row[1])
 
-    print "--------- x points ---------"
-    print x_points
-    print "--------- y points ---------"
-    print y_points
 
     x_mean = get_mean(x_points)
     y_mean = get_mean(y_points)
 
-    print "--------- x mean ---------"
-    print x_mean
-    print "--------- y mean ---------"
-    print y_mean
-
     total = 0
     i = 0
     while i < len(x_points):
-        print("----------x_point[i]--------------")
-        print(x_points[i])
-        print("-------------x mean---------------")
-        print(x_mean)
         total += get_covariance(x_points,y_points) / get_variance(x_points)
-        # total = total + ((x_points[i] - x_mean) * (y_points[i] - y_mean) / ((x_points[i] - x_mean) ** 2))
         i+=1
 
     m = total
@@ -101,19 +88,24 @@ def simple_linear_regression(train, test):
     m = h['m']
     b = h['b']
 
+    print "------------ coeficient M -------"
+    print m
+    print "------------ coeficient B -------"
+    print b
+
     for row in test:
         yhat = m * row[0] + b
         prediction.append(yhat)
     return prediction
 
 
-train = [[108,392.5],[19,46.2],[13,15.7],[124,422.2],[40,119.4],[57,170.9],[23,56.9],[14,77.5],[45,21410,65.3],[5,20.9],[48,248.1],[11,23.5],[23,39.6],[7,48.8],[2,6.6],
-         [24,134.9],[6,50.9],[3,4.4],[23,1136,14.8],[9,48.7],[9,52.1],[3,13.2],[29,103.9],[7,77.5],[4,11.8],[20,98.1],[7,27.9],[4,38.1],[0,025,69.2],[6,14.6],[5,40.3],
+train = [[108,392.5],[19,46.2],[13,15.7],[124,422.2],[40,119.4],[57,170.9],[23,56.9],[14,77.5],[45,214],[10,65.3],[5,20.9],[48,248.1],[11,23.5],[23,39.6],[7,48.8],[2,6.6],
+         [24,134.9],[6,50.9],[3,4.4],[23,136],[20,14.8],[9,48.7],[9,52.1],[3,13.2],[29,103.9],[7,77.5],[4,11.8],[20,98.1],[7,27.9],[4,38.1],[10,25],[15,69.2],[6,14.6],[5,40.3],
          [22,161.5],[11,57.2],[61,217.6],[12,58.1],[4,12.6],[16,59.6],[13,89.9],[60,202.4],[41,181.3],[37,152.8],[55,162.8],[41,73.4],[11,21.3],[27,92.6],[8,76.1],
-         [3,39.9],[17,142.1],[13,9313,31.9],[15,32.1],[8,55.6],[29,133.3],[30,194.5],[24,137.9],[9,87.4],[31,209.8],[14,95.5],[53,244.6],[26,187.5]]
+         [3,39.9],[17,142.1],[13,93],[23,31.9],[15,32.1],[8,55.6],[29,133.3],[30,194.5],[24,137.9],[9,87.4],[31,209.8],[14,95.5],[53,244.6],[26,187.5]]
 
 prediction = simple_linear_regression(train,train)
-
+#
 print "----------- prediction --------------"
 print prediction
 print "-------------------------"
@@ -121,15 +113,13 @@ print "-------------------------"
 y_points = []
 x_points = []
 
-for x in range(len(train)):
-    x_points.append(x)
+for row in train:
+    x_points.append(row[0])
     # Plot the data
-    y_points.append(prediction[x])
+    y_points.append(row[1])
 
 plt.scatter(x_points, y_points, color='red', marker='^')
 plt.scatter(x_points, prediction, color='blue', marker='^')
-# # Add a legend
-plt.legend()
 
 # # Show the plot
 plt.show()
